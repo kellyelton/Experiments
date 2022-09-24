5ef77f747e9ab2f0ccd8b9c48c82e1d7d22b4579
Kelly Elton <its.the.doc@gmail.com>
2022-09-24 02:42:26 -0500

    mutate more often than new nets

--- src/CS001/VNet/VNetApp/ProminentColorSmall/ProminentColorSmallViewModel.cs
+++ src/CS001/VNet/VNetApp/ProminentColorSmall/ProminentColorSmallViewModel.cs
@@ -96,7 +96,7 @@ public class ProminentColorSmallViewModel : ViewModel
             var tasks = Enumerable
                 .Range(0, 500)
                 .Select(_ => {
-                    if (best_brains.Count > 10 && Random.Shared.NextDouble() > 0.4) {
+                    if (best_brains.Count > 10 && Random.Shared.NextDouble() > 0.1) {
                         var ri = Random.Shared.Next(0, best_brains.Count);
                         var r = best_brains.Skip(ri).Take(1).Single();
                         return MutateNet(r);
